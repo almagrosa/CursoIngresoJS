2604d80ba920f21bd34ea700889c959ff7b4f7d8
almagrosa <adriancostantino908@hotmail.com>
2016-11-14 19:43:49 -0300

    clase 1

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	
+	var nombre
+	nombre=document.getElementById('elNombre').value;
+	var edad
+	edad=document.getElementById('laEdad').value;
+	alert("usted se llama: "+nombre+" y tiene: "+edad+" siglos");
+}
+
